4a0235a1f606b011b4123a2c6dbcf9954601dbbc
Nick <nep8189@lockhaven.edu>
2018-12-09 18:11:22 -0500

    Staged changes

--- Dog Show/src/graphics/Renderer.java
+++ Dog Show/src/graphics/Renderer.java
@@ -1,13 +1,14 @@
 package graphics;
 
 import java.awt.Canvas;
+import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
 import listeners.GraphicsListener;
 
-public final class Renderer extends JFrame
+public final class Renderer extends JFrame implements Cloneable
 {
 	private static final long serialVersionUID = 1L;
 	
@@ -18,7 +19,6 @@ public final class Renderer extends JFrame
 		//Initial JFrame setup
 		super("Dog Show Admin Tool");
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		super.setIgnoreRepaint(true);
 		super.setResizable(false);
 		
 		//Initial Canvas setup
@@ -28,7 +28,7 @@ public final class Renderer extends JFrame
 		//Final setup
 		super.add(canvas);
 		super.pack();
-		canvas.createBufferStrategy(2);
+		canvas.createBufferStrategy(1);
 		super.setVisible(true);
 	}
 	
@@ -40,5 +40,16 @@ public final class Renderer extends JFrame
 	public Canvas getCanvas()
 	{
 		return canvas;
+	}
+	
+	public void addComponent(Component component)
+	{
+		super.add(component);
+		component.setVisible(true);
+		
+		super.add(canvas);
+		super.pack();
+		super.setVisible(true);
+		super.repaint();
 	}
 }
--- Dog Show/src/menus/MainMenu.java
+++ Dog Show/src/menus/MainMenu.java
@@ -1,7 +1,5 @@
 package menus;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferStrategy;
@@ -9,9 +7,7 @@ import java.io.IOException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
-import javax.swing.AbstractButton;
 import javax.swing.JButton;
-import javax.swing.JPanel;
 
 import core.Status;
 import graphics.Renderer;
@@ -42,6 +38,7 @@ public class MainMenu implements GraphicsListener
 		}
 		
 		checkStatus();
+		addButtons();
 		renderer.makeGraphicsCalls(this);
 	}
 	
@@ -62,32 +59,31 @@ public class MainMenu implements GraphicsListener
 			thread.start();
 		}
 	}
+	
+	private void addButtons()
+	{
+		JButton button = new JButton("Register");
+		button.setBounds(0, 0, 400, 320);
+		button.setText("test");
+		renderer.addComponent(button);
+	}
 
 	@Override
 	public void graphicsCall(Renderer renderer)
 	{
-		BufferStrategy strategy = renderer.getCanvas().getBufferStrategy();
-		Graphics2D gfx = (Graphics2D) strategy.getDrawGraphics();
-		
-		gfx.clearRect(0, 0, renderer.getWidth(), renderer.getHeight());
-		
-		gfx.drawImage(dog, 0, 0, renderer.getCanvas().getWidth(), renderer.getCanvas().getHeight(), 
-				0, 0, dog.getWidth(null), dog.getHeight(null), null);
-		
-		JPanel buttonPanel = new JPanel();
-		JButton registerPageBtn = new JButton("heck");
-		registerPageBtn.setBounds(20, 20, 200, 60);
-		registerPageBtn.setLocation(200, 200);
-		registerPageBtn.setVerticalTextPosition(AbstractButton.CENTER);
-		registerPageBtn.setHorizontalTextPosition(AbstractButton.LEADING);
-		registerPageBtn.setText("fuck");
-		renderer.add(registerPageBtn);
-		registerPageBtn.setVisible(true);
-		//registerPageBtn.paint(gfx);
-		buttonPanel.setVisible(true);
-		renderer.add(buttonPanel);
-		
-		gfx.dispose();
-		strategy.show();
+		int i = 0;
+		while(i++ < 13)
+		{
+			BufferStrategy strategy = renderer.getCanvas().getBufferStrategy();
+			Graphics2D gfx = (Graphics2D) strategy.getDrawGraphics();
+			
+			gfx.clearRect(0, 0, renderer.getWidth(), renderer.getHeight());
+			
+			gfx.drawImage(dog, 0, 0, renderer.getCanvas().getWidth(), renderer.getCanvas().getHeight(), 
+					0, 0, dog.getWidth(null), dog.getHeight(null), null);
+			
+			gfx.dispose();
+			strategy.show();
+		}
 	}
 }
